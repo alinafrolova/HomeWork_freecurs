190f4cd8fcc2db0f966a9a789abdb05c1cbaea4b
Alina <alinkamog@gmail.com>
2017-03-05 16:07:44 +0200

    Fixed createList
    Now it's work correctly
    for checking
     (function () {
            console.log(createList(['мясо', 'рыба']));
            console.log(createList(['мясо', ['яблоки', 'бананы']], 'ol') );
            console.log(createList(['мясо', ['яблоки', 'бананы']], 'div', 'div'));
        }())

--- 15/createList.js
+++ 15/createList.js
@@ -1,32 +1,36 @@
 /**
- * Created by Frolova on 02.03.2017.
- * Выполнить
- Релизовать функцию createList(listData, listContainer, itemContainer), возвращаюшую узел списка.
- Использовать innerHTML нельзя. Второй и третий аргументы не обязательные. Значения по умолчанию для них - ul и li. listData - массив.
- listData Может содержать как элементы (текст), так и массивы элементов. Глубина вложенности массивов любая.
+ * Created by иарвар on 05.03.2017.
  */
 function createList(listData, listContainer, itemContainer) {
-    var newOl = document.createElement(listContainer || 'ol'), //создаем строку ol
-        newLi = [];
-    itemContainer = itemContainer || 'li';
-        for (var i = 0; i < listData.length; i++){
-            newLi[i] = document.createElement(itemContainer);
-            newLi[i].appendChild(document.createTextNode(listData[i]));
-            document.body.appendChild(newOl);
-            var newArray = listData[i];
-              if (isArray(newArray)){ createList(newArray, listContainer, itemContainer); return newOl;}
-            newOl.appendChild(newLi[i]);
+    var newOl = document.createElement(listContainer || 'ul'); //создаем строку ol
+    document.body.appendChild(newOl);
+    for (var i = 0; i < listData.length; i++){
+        var newArray = listData[i];
+        createNode(newArray,newOl, listContainer, itemContainer);}
+}
+function createNode(newArray,newOl, listContainer, itemContainer) {
+        itemContainer = itemContainer || 'li',
+        newLi = document.createElement(itemContainer);
+        newOl.appendChild(newLi);
+        if (isArray(newArray)){
+           var arrayOl = document.createElement(listContainer || 'ul');
+              for (var j = 0; j < newArray.length; j++){
+                var arrayLi = [];
+                arrayLi[j] = document.createElement(itemContainer);
+                arrayLi[j].appendChild(document.createTextNode(newArray[j]));
+                arrayOl.appendChild(arrayLi[j]);
+                newLi.appendChild(arrayOl);
+            }
         }
-      return newOl;
-    }
-  
+             else {
+            newLi.appendChild(document.createTextNode(newArray));
+        }
 
+    return newOl;
+}
 function isArray(array) {
     if (array instanceof Array) {
         return true;
     }
     return false;
 }
-
-
-
