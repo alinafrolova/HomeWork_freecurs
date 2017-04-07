741bdbac9c0fa686eee08ac352fa075d1b29e413
Alina <alinkamog@gmail.com>
2017-04-07 16:03:41 +0300

    Create Struct

--- 20/20-regexp/task/form-validation/js/validate.js
+++ 20/20-regexp/task/form-validation/js/validate.js
@@ -1,1 +1,60 @@
+'use strict';
+(function () {
+    function CustomValidation() {
+        var FORM = document.getElementById("form"),
+            SUBMIT = document.getElementById("submit")
+    
+        //взаимодействие с формой
+        FORM.addEventListener('input', this._checkErrors.bind(this));
+        SUBMIT.addEventListener('input', this._checkSubmit.bind(this));
+    }
+    
+   
+    CustomValidation.prototype._checkErrors = function(event) {
+        console.log("checkErrors");
+         var target = event.target;
+         
+       switch (target.id){
+            case 'email' : checkEmail(); break;
+            case 'password' : checkPass(); break;
+            case 'city' : checkCity(); break;
+            case 'phone' : checkPhone(); break;
+        }
+        
+        
+    }
+    CustomValidation.prototype._checkSubmit = function(input) {
+            console.log("_checkSubmit");
+        }
+    
+   function checkEmail() {
+       console.log("checkEmail");
+   }
+    function checkPass() {
+        console.log("checkPass");
+    }
+    function checkCity() {
+        console.log("checkCity");
+    }
+    function checkPhone() {
+        console.log("checkPhone");
+    }
+        
+    function checkValidity(e) {
+        var inputs = document.getElementsByTagName("input");
+        for (var i = 0; i < inputs.length; i++) {
+        
+            var input = inputs[i];
+        
+            // Проверим валидность поля, используя встроенную в JavaScript функцию checkValidity()
+            if (!checkValidity(input)) {
+                console.log("checkSubmit");
+              
+            }
+        
+        }
+     }
+   
+   window.CustomValidation = CustomValidation;
+}());
 
