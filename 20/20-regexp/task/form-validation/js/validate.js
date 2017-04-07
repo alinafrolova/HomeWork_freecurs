34c60ad2af93f60adfc46d030eb29ee9ad563815
Alina <alinkamog@gmail.com>
2017-04-07 17:33:30 +0300

    Add regExp

--- 20/20-regexp/task/form-validation/js/validate.js
+++ 20/20-regexp/task/form-validation/js/validate.js
@@ -1,8 +1,10 @@
 'use strict';
 (function () {
+    var regexp,
+        arrayEmails;
     function CustomValidation() {
         var FORM = document.getElementById("form"),
-            SUBMIT = document.getElementById("submit")
+            SUBMIT = document.getElementById("submit");
     
         //взаимодействие с формой
         FORM.addEventListener('input', this._checkErrors.bind(this));
@@ -12,13 +14,14 @@
    
     CustomValidation.prototype._checkErrors = function(event) {
         console.log("checkErrors");
-         var target = event.target;
+         var target = event.target,
+             value = target.value;
          
        switch (target.id){
-            case 'email' : checkEmail(); break;
-            case 'password' : checkPass(); break;
-            case 'city' : checkCity(); break;
-            case 'phone' : checkPhone(); break;
+            case 'email' : checkEmail(value); break;
+            case 'password' : checkPass(value); break;
+            case 'city' : checkCity(value); break;
+            case 'phone' : checkPhone(value); break;
         }
         
         
@@ -27,19 +30,38 @@
             console.log("_checkSubmit");
         }
     
-   function checkEmail() {
+   function checkEmail(value) {
        console.log("checkEmail");
+       regexp = /[\D||\d][@][a-z][(?=(\.)][a-z]/;
+       if (!regexp.test(value)){console.log("The email is incorect");} //Ошибка в email-е
    }
-    function checkPass() {
+    function checkPass(value) {
+        /*Пароль содержит запрещенные символы (разрешенные - латинские буквы, цифры, подчеркивание, минус)*/
         console.log("checkPass");
-    }
-    function checkCity() {
-        console.log("checkCity");
-    }
-    function checkPhone() {
-        console.log("checkPhone");
-    }
+        var regexpSymbols = /^[a-zA-Z0-9]{5,}/, //more than 5 symbols
+            regexpChaNum = /[\D+)][\d+]/,
+            regexpNum = /[\d+]/,
+            regexpIncorData= /[(а-я)||_||-]/;
+        if (!regexpSymbols.test(value)){console.log("The password is shot, should be more than 5 symbols");} //Пароль слишком короток (до 5 символов)
+        if (!regexpChaNum.test(value)){ //Простой пароль
+            if (!regexpNum.test(value)){console.log("The password hasn't characters, only numbers");} //только числа
+            else {console.log("The password hasn't numbers, only characters");} //только буквы
+        }
+        if (!regexpIncorData.test(value)){console.log("The password has prohibited symbols ");}//Пароль содержит запрещенные символы (разрешенные - латинские буквы, подчеркивание, минус)
         
+    }
+    function checkCity(value) {
+        console.log("checkCity");
+         regexp = /^[a-zA-Z]/;
+        if (!regexp.test(value)){console.log("The city is incorect");}
+    }
+    function checkPhone(value) {
+        console.log("checkPhone");
+        regexp = /\+38\s(\d){2,3}\s(\d){3}\s(\d){2}\s(\d){2}/;
+        if (!regexp.test(value)){console.log("The numbet is incorect ");}//Международный формат записи телефона не выдержан
+    }
+    
+    
     function checkValidity(e) {
         var inputs = document.getElementsByTagName("input");
         for (var i = 0; i < inputs.length; i++) {
