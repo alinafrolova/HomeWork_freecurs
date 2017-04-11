f7854592dbbd68d1b2407d9c2d7341791cd8cab3
Alina <alinkamog@gmail.com>
2017-04-11 16:23:58 +0300

    To fix some moment

--- 20/20-regexp/task/form-validation/js/validate.js
+++ 20/20-regexp/task/form-validation/js/validate.js
@@ -7,7 +7,7 @@
             SUBMIT = document.getElementById("submit");
     
         //взаимодействие с формой
-        FORM.addEventListener('input', this._checkErrors.bind(this));
+        FORM.addEventListener('keyup', this._checkErrors.bind(this));
         SUBMIT.addEventListener('click', this._checkSubmit.bind(this));
     }
     
@@ -17,9 +17,9 @@
          var target = event.target,
              value = target.value,
              result;
-        checkEmpty(event) ;
+        checkEmpty(target) ;
        switch (target.id){
-            case 'email' : setInterval(function() { checkEmail(value);}, 1000);  break;
+            case 'email' : checkEmail(value);  break;
             case 'password' : checkPass(value); break;
             case 'city' : checkCity(value); break;
             case 'phone' : checkPhone(value); break;
@@ -36,8 +36,8 @@
    function checkEmail(value) {
        console.log("checkEmail");
        regexp = /[\D||\d][@][a-z]{1,}[(?=(\.)][a-z]/;
-       if (!regexp.test(value)){addClasses('.email'); return false;} //Ошибка в email-е
-       else {return true;}
+       if (!regexp.test(value)){addClasses('.email'); } //Ошибка в email-е
+       else {deleteClasses('.email');}
    }
     function checkPass(value) {
         /*Пароль содержит запрещенные символы (разрешенные - латинские буквы, цифры, подчеркивание, минус)*/
@@ -46,33 +46,51 @@
             regexpChaNum = /[\D+)][\d+]/,
             regexpNum = /[\d+]/,
             regexpIncorData= /[(а-я)||_||-]/;
-        if (!regexpSymbols.test(value)){addClasses('.password-shot');} //Пароль слишком короток (до 5 символов)
-        if (!regexpChaNum.test(value)){ //Простой пароль
-            if (!regexpNum.test(value)){ addClasses('.password-characters');} //только числа
-            else { addClasses('.password-numbers');} //только буквы
+        if (!regexpSymbols.test(value)){
+            addClasses('.password-shot');}
+           else{ deleteClasses('.password-shot');} //Пароль слишком короток (до 5 символов)
+        if (!regexpChaNum.test(value)||!regexpNum.test(value)){ //Простой пароль
+            addClasses('.password-characters');//только числа
+            addClasses('.password-numbers'); //только буквы
+        }else{
+            deleteClasses('.password-characters');//только числа
+            deleteClasses('.password-numbers'); //только буквы
         }
-        if (!regexpIncorData.test(value)){ addClasses('.password-prohibited');}//Пароль содержит запрещенные символы (разрешенные - латинские буквы, подчеркивание, минус)
         
+        if (regexpIncorData.test(value)){
+            addClasses('.password-prohibited');}//Пароль содержит запрещенные символы (разрешенные - латинские буквы, подчеркивание, минус)
+        else {deleteClasses('.password-prohibited');}
     }
     function checkCity(value) {
         console.log("checkCity");
          regexp = /^[a-zA-Z]/;
         if (!regexp.test(value)){ addClasses('.city');}
+        else {deleteClasses('.city');}
     }
     function checkPhone(value) {
         console.log("checkPhone");
         regexp = /\+38\s(\d){2,3}\s(\d){3}\s(\d){2}\s(\d){2}/;
         if (!regexp.test(value)){ addClasses('.phone');}//Международный формат записи телефона не выдержан
+        else {deleteClasses('.phone');}
     }
     function addClasses(where) {
         
         document.querySelector(where).classList.add("show-error");
-        document.querySelector(where).classList.add("has-error");
+        document.querySelector(where).style.display = 'block';
         
     }
-    function checkEmpty(event) {
-        var value = event.target.value;
-        if (!value){console.log("The field is empty");}
+    function deleteClasses(where) {
+        document.querySelector(where).style.display = 'none';
+        
+    }
+    function checkEmpty(input) {
+        
+        if (!input.value|| input.value === ' '){
+            console.log("The field is empty");
+            input.nextElementSibling.style.display = 'block';
+        }else {
+            input.nextElementSibling.style.display = 'none';
+        }
     }
     function checkValidity(event) {
         var inputs,input, attrs;
@@ -82,10 +100,7 @@
              attrs = input.attributes; // (4) можно получить коллекцию атрибутов
             for (var j = 0; j < attrs.length; j++) {
                 if (attrs[j].name === 'required' ) {
-                    if (!input.value){
-                        console.log("The field is empty");
-                        input.nextElementSibling.style.display = 'block';
-                    }
+                    checkEmpty(input) ;
                 }
             }
                         
