ca0bb5acb01948f39259ac38cd8e93323270e8c0
Alina <alinkamog@gmail.com>
2017-04-20 17:18:01 +0300

    Not good start
    change image

--- 22/js/slider.js
+++ 22/js/slider.js
@@ -2,23 +2,39 @@
  * Created by Frolova on 12.04.2017.
  */
 'use strict';
-    var animateSlider = {
-       slides:['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg', 'images/image4.jpg'],
-       index: 0,
-       set: function () {
-           document.getElementById("images-panel").style.backgroundImage = "url("+image+")";
-           console.log("set slider");
-       },
-        init: function(){
-            console.log("start");
-
-        },
-        changeSlider: function () {
-            console.log("change slider");
+  
+    (function () {
+        var slides = ['images/image1.jpg', 'images/image2.jpg',  'images/image3.jpg', 'images/image4.jpg'],
+            image = jQuery('div').find('img');
+         function AnimateSlider() {
+             this.image = image;
+             document.addEventListener("DOMContentLoaded", this.init.bind(this), false);
+             document.addEventListener("click", this.changeSlider.bind(this), false);
         }
-
-    }
-
+        AnimateSlider.prototype.set = function () {
+          
+        }
+        AnimateSlider.prototype.init = function () {
+            console.log("init slider");
+            jQuery.each(slides, function(index) {
+                setTimeout(flippingImages(index), 500000);
+        
+            });
+           
+        }
+        AnimateSlider.prototype.changeSlider = function (event) {
+            console.log("change slider");
+            
+            
+        }
+        window.AnimateSlider = AnimateSlider;
+         
+         function flippingImages(index) {
+             if (index === 3) { index = -1;}
+             console.log("change slider" + index);
+                 var result = jQuery('div').find('img').attr("src", slides[index + 1]);
+         }
+    }());
     
 
 
