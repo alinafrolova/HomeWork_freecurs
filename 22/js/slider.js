728cca4c257041f41a27ad027bc7f27996e2378c
Alina <alinkamog@gmail.com>
2017-04-21 13:28:40 +0300

    Change slide auto, but not ideal

--- 22/js/slider.js
+++ 22/js/slider.js
@@ -7,38 +7,37 @@
         var slides = ['images/image1.jpg', 'images/image2.jpg',  'images/image3.jpg', 'images/image4.jpg'];
            
          function AnimateSlider() {
-                          
-            // document.addEventListener("DOMContentLoaded", this.init.bind(this), false);
+             
+             document.addEventListener("DOMContentLoaded", this.autoplay.bind(this), false);
              document.addEventListener("click", this.changeSlider.bind(this), false);
         }
         AnimateSlider.prototype.set = function () {
           
         }
-        AnimateSlider.prototype.init = function () {
+        AnimateSlider.prototype.autoplay = function () {
+            var image  = jQuery('div').find('img');
             console.log("init slider");
             jQuery.each(slides, function(index) {
-                setTimeout(flippingImages(index), 500000);
-        
+                
+                image.animate( {left:'+=100'},function(){
+                    if (index === 3) { index = -1;}
+                    image.attr("src", slides[index + 1]);
+                });
+                
             });
            
         }
         AnimateSlider.prototype.changeSlider = function (event) {
-            var image  = jQuery('div').find('img');
-            console.log("change slider");
-            var target = event.target, // где был клик?
+            var image  = jQuery('div').find('img'),
+                target = event.target,
                 index = target.textContent;
-            console.log(index);
+            
             if (target.tagName !== "DIV") return;
             image.attr("src", slides[index - 1]);
             
         }
         window.AnimateSlider = AnimateSlider;
-         
-         function flippingImages(index) {
-             if (index === 3) { index = -1;}
-             console.log("change slider" + index);
-                 var result = jQuery('div').find('img').attr("src", slides[index + 1]);
-         }
+                 
     }());
     
 
