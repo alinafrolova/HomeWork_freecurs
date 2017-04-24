62dd74e72fe37673db7b168af5ee2b3598fc8f15
Alina <alinkamog@gmail.com>
2017-04-24 17:09:00 +0300

    Some animation

--- 22/js/slider.js
+++ 22/js/slider.js
@@ -2,7 +2,7 @@
  * Created by Frolova on 12.04.2017.
  */
 'use strict';
-  
+  //.chisel:active
     (function () {
         var slides = ['images/image1.jpg', 'images/image2.jpg',  'images/image3.jpg', 'images/image4.jpg'];
            
@@ -15,29 +15,48 @@
           
         }
         AnimateSlider.prototype.autoplay = function () {
-            var image  = jQuery('div').find('img');
+           var numEleme,
+               index = 0;
             console.log("init slider");
-            jQuery.each(slides, function(index) {
-                
-                image.animate( {left:'+=100'},function(){
-                    if (index === 3) { index = -1;}
-                    image.attr("src", slides[index + 1]);
-                });
-                
-            });
-           
+            numEleme = animateSlide(index);
+            jQuery('.chisel').eq(numEleme).removeClass("chisel-active");
         }
         AnimateSlider.prototype.changeSlider = function (event) {
-            var image  = jQuery('div').find('img'),
-                target = event.target,
-                index = target.textContent;
-            
-            if (target.tagName !== "DIV") return;
-            image.attr("src", slides[index - 1]);
-            
+            var numEleme;
+            setTimeout(numEleme = slideSlider(event), 2000);
+            animateSlide(numEleme);
         }
         window.AnimateSlider = AnimateSlider;
-                 
+         
+        function animateSlide(index) {
+            index = index || 0
+            var image  = jQuery('div').find('img');
+            jQuery.each(slides,index,function(index) {
+                
+                image.animate({opacity: "1"}, function () {jQuery('.chisel').eq(index).addClass("chisel-active") })
+                    .animate({opacity: "0.8"}, 1500)
+                    .animate({opacity: "0.2"}, function () {jQuery('.chisel').eq(index).removeClass("chisel-active"); })
+                    .animate( {left:'+=100'},function(){if (index === 3 ) { index = -1;}  image.attr("src", slides[index + 1]); })
+                    .animate({opacity: "0.4"}, "slow");
+        
+            });
+            return index;
+        }
+        function slideSlider(event) {
+            var image  = jQuery('div').find('img'),
+                target = event.target,
+                index = target.textContent - 1;
+    
+            if (target.tagName !== "DIV") return;
+            jQuery('div').find('.chisel-active').removeClass("chisel-active");
+            image.animate({opacity: "0.1"}, function () {jQuery('.chisel').eq(index,index).addClass("chisel-active") })
+                .animate({opacity: "0.4"}, 1500)
+                .animate( {left:'+=100'},function(){image.attr("src", slides[index]);})
+                .animate({opacity: "0.6"}, function () {jQuery('.chisel').eq(index,index).removeClass("chisel-active"); })
+                .animate({opacity: "1"}, "slow");
+            return index;
+        }
+        
     }());
     
 
