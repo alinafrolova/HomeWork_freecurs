59902230072f48a54dc76dba5de32e5c1d89681a
Alina <alinkamog@gmail.com>
2017-05-03 11:10:59 +0300

    example for SoftServ
    https://softserve.ua/wp-content/uploads/2014/02/Web-UI1_2016_01_21_11_02_55_367.pdf

---
+++ table_html by SoftServ/max.js
@@ -0,0 +1,11 @@
+/**
+ * Created by иарвар on 03.05.2017.
+ */
+'use strict';
+(function () {
+    var MyArray = [200,15,-2,500,-100,0];
+    function getMax(array) {
+        return Math.max.apply(null, MyArray);
+    }
+
+})();
