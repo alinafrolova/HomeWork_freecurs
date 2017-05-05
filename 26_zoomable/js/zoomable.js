6dfc1c531f5e02df460dc82d6a97bba65458dfed
Alina <alinkamog@gmail.com>
2017-05-05 11:15:39 +0300

    The last point isn't released
    zoomable
    - при клике по изображению показывать увеличенное изображение, затемняя всю страницу
    кликнуть по содежимому страницы, выделить текст мышкой не должно быть возможно
    - увеличенное изображение всегда отцентрировано по вертикали и горизонтали экрана
    (даже при изменении размеров страницы)
    - путь увеличенного изображения отличается от пути превью только названием папки.
    Например:
    img/small/dog.jpg
    img/large/dog.jpg
    - увеличенное изображение скрывается при клике на крестике или при нажатии esc
    - кнопка закрытия всегда находится в правом верхнем уг�

--- 26_zoomable/js/zoomable.js
+++ 26_zoomable/js/zoomable.js
@@ -4,18 +4,53 @@
 'use strict';
 
    function makeZoomable(set) {
+       var content  = $( ".content" );
+
        $(document).ready(function () {
-           $('img').click(function () {
-               var srcImg = $(this).attr("src"),
-                   newSrc = srcImg.replace(/small/g,"large");
-              $(this).animate({opacity:0},function(){
-                  $( ".content" ).addClass( "dark" );
-                  $(".image").addClass( "active_img" );
-                  $(".image").find("img").attr("src", newSrc);
+           $('img').on('click', function () {
+               content.after('<div class = "image"><img src="img/small/pigeon.jpg" alt=""><span class="close-block"></span></div>');
+               var src = $(this).attr("src"),
+                   blockImg = $(".image"),
+                   bigImg = blockImg.find("img");
+
+               bigImg.animate({opacity:1},function(){
+                  content.addClass( "dark" );
+                  bigImg.attr("src", src);
+                   var   newSrc = bigImg.attr("src").replace(/small/g,"large");
+                  bigImg.attr("src", newSrc);
+                  blockImg.addClass( "active_img" ).center();
                   });
 
+            });
 
-           })
+           $(this).click(function(event){
+               if (event.target.tagName  === 'SPAN')
+                   closeImg();
+           });
+           $(this).keydown(function(eventObject){
+               if (eventObject.which == 27)
+                   closeImg();
+           });
        });
+
+       function closeImg() {
+           var blockImg = $(".image"),
+               bigImg = blockImg.find("img");
+
+           bigImg.animate({opacity:1},function(){
+               blockImg.remove();
+               content.removeClass( "dark" );
+           });
+       }
+       jQuery.fn.center = function () {
+           this.css("position","absolute");
+           var resTop = (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop(),
+               resLeft = (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft();
+           if( resTop < 0) {resTop = 10}
+           if( resLeft < 0) {resLeft = 10}
+           this.css("top", resTop + "px");
+           this.css("left", resLeft + "px");
+           return this;
+       }
        }
 
