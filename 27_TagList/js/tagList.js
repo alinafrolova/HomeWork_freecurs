4a9f0f8ff19fe75ad9135cafb2b337d70b4f0900
Alina <alinkamog@gmail.com>
2017-05-08 21:33:17 +0300

    Add files via upload

---
+++ 27_TagList/js/tagList.js
@@ -0,0 +1,26 @@
+/**
+ * Created by иарвар on 05.05.2017.
+ */
+
+'use strict';
+(function () {
+    function TagList(){
+        
+    }
+    window.TagList = TagList;
+    TagList.prototype._createFrame = function () {
+
+    }
+    TagList.prototype._AddNode = function () {
+
+    }
+    TagList.prototype._ShowNode = function () {
+
+    }
+    TagList.prototype._EditNode = function () {
+
+    }
+    TagList.prototype._DeleteNode = function () {
+
+    }
+})();
